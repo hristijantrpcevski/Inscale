a82248854de7e1687a2119664415751020fcc86f
hristijantrpchevski <hristijan.trpchevski@eriden.io>
2021-09-21 18:32:26 +0200

    added info box

--- recruitment-react-master/my-app/src/Components/Users/User/User.tsx
+++ recruitment-react-master/my-app/src/Components/Users/User/User.tsx
@@ -1,15 +1,23 @@
 import React from "react";
-
 import { Marker } from "@react-google-maps/api";
 
 interface UserProps {
   user: any;
+  onSelectUser: (user: any) => void;
 }
 
-const User: React.FC<UserProps> = ({ user }) => {
-  //   console.log(user, "USER");
+const User: React.FC<UserProps> = ({ user, onSelectUser }) => {
+  const { id, position, listingsCount, userTypeIdentifier, primaryTradeType } =
+    user;
 
-  return <Marker key={user.id} position={user.position} />;
+  return (
+    <Marker
+      key={id}
+      position={position}
+      label={listingsCount.toString()}
+      onClick={() => onSelectUser(user)}
+    />
+  );
 };
 
 export default User;
--- recruitment-react-master/my-app/src/Components/Users/Users.tsx
+++ recruitment-react-master/my-app/src/Components/Users/Users.tsx
@@ -1,30 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import User from "./User/User";
 import { getAllUsers } from "../../Redux/Actions/users";
-import { CircularProgress } from "@material-ui/core";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { CircularProgress, Typography } from "@material-ui/core";
+import { GoogleMap, LoadScript, InfoWindow } from "@react-google-maps/api";
 
 interface Props {}
-const Users: React.FC<Props> = ({}) => {
+const mapStyles = {
+  height: "100vh",
+  width: "100%",
+};
+
+const defaultCenter = {
+  lat: 50.3851,
+  lng: 10.1734,
+};
+const Users: React.FC<Props> = () => {
   const dispatch = useDispatch();
 
   const users = useSelector((state: any) => state.usersReducer.users);
   const loadingUsers = useSelector((state: any) => state.usersReducer.loading);
-
+  const [selectedUser, setSelectedUser] = useState<any>(null);
   useEffect(() => {
     dispatch(getAllUsers());
   }, [dispatch]);
 
-  const mapStyles = {
-    height: "100vh",
-    width: "100%",
+  const onSelectUser = (user: any) => {
+    setSelectedUser(user);
   };
-
-  const defaultCenter = {
-    lat: 50.3851,
-    lng: 10.1734,
+  const showInformation = (info: any, name: string) => {
+    return info && info.length > 0 ? (
+      <Typography>
+        <strong>{name}</strong> {info}
+      </Typography>
+    ) : null;
   };
   return (
     <Container>
@@ -40,8 +50,39 @@ const Users: React.FC<Props> = ({}) => {
             center={defaultCenter}
           >
             {users.map((user: any) => (
-              <User user={user} />
+              <User user={user} key={user.id} onSelectUser={onSelectUser} />
             ))}
+            {selectedUser && (
+              <InfoWindow
+                position={selectedUser.position}
+                onCloseClick={() => onSelectUser(null)}
+              >
+                <div>
+                  {selectedUser.company.logoPath ? (
+                    <Typography>
+                      <img
+                        src={selectedUser.company.logoPath}
+                        alt="Logo"
+                        width="50"
+                      />
+                    </Typography>
+                  ) : null}
+                  {showInformation(selectedUser.company.name, "Company name:")}
+                  {showInformation(
+                    selectedUser.company.translatedUserType,
+                    "User Type:"
+                  )}
+                  {showInformation(
+                    selectedUser.company.agricultureTypes,
+                    "Agriculture Types:"
+                  )}
+                  {showInformation(
+                    selectedUser.company.productionTypes,
+                    "Production Types:"
+                  )}
+                </div>
+              </InfoWindow>
+            )}
           </GoogleMap>
         </LoadScript>
       )}
